fdb06d6df632d2559bb5a3f048c690f99702fafd
nk3750 <neelabhkumar@outlook.com>
2024-03-29 16:48:44 -0700

    Add unit conversion

--- appleHealthBot.py
+++ appleHealthBot.py
@@ -45,13 +45,14 @@ def load_data_into_db(csv_path, db_path, table_name):
 
 def main():
     system_prompt = """You are an agent designed to interact with a SQL database. Given an input question, create a 
-    syntactically correct {dialect} query to run, then look at the results of the query and return the answer. You 
+    syntactically correct  query to run, then look at the results of the query and return the answer. You 
     can order the results by a relevant column to return the most interesting examples in the database.When the user 
     refers to a relative timeline such as last week, last month, compare it with the current system time. Never query 
     for all the columns from a specific table, only ask for the relevant columns given the question. You have access 
     to tools for interacting with the database. Only use the given tools. Only use the information returned by the 
     tools to construct your final answer. You MUST double check your query before executing it. If you get an error 
-    while executing a query, rewrite the query and try again.
+    while executing a query, rewrite the query and try again. The exerciseTime db contains the time in minutes, 
+    not hours, make sure you do proper unit conversions before answering the query.
 
 DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
