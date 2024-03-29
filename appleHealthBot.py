772fa47a06e75489642ebeecbd1275b5de88759b
nk3750 <neelabhkumar@outlook.com>
2024-03-29 13:13:24 -0700

    Try another model

--- appleHealthBot.py
+++ appleHealthBot.py
@@ -11,7 +11,7 @@ from openai import OpenAI
 
 def classify_query(query, openai_api_key):
     client = OpenAI(api_key=openai_api_key)
-    response = client.chat.completions.create(model="gpt-3.5-turbo-instruct",
+    response = client.chat.completions.create(model="gpt-3.5-turbo",
                                          messages=[
                                              {"role": "system",
                                               "content": "Classify the following query into categories 'workouts', 'sleep', or 'other'. The contect should only contain the :\n{query}"}
